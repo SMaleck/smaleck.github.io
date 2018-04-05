1748362b8f1969304f8ea3942a5fd90ce9de3ca6
Sebastian Maleck <s.maleck@outlook.de>
2018-04-05 16:33:18 +0200

    Adds Boilerplate

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+    el: '#app',
+    
+    data: {
+        skillGroups:[
+            {
+                title: "Programming Languages",
+                items:[
+                    { 
+                        name: "C#",
+                        level: 9
+                    },
+                    { 
+                        name: "JavaScript",
+                        level: 8
+                    },
+                    { 
+                        name: "Java",
+                        level: 6
+                    }
+                ]
+            }
+        ]
+    }
+  })
