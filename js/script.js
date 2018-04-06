521eb334bcf8e3320c4429537ca714f7b0cb5d22
Sebastian Maleck <s.maleck@outlook.de>
2018-04-06 15:48:40 +0200

    Adds skill matrix

--- js/script.js
+++ js/script.js
@@ -1,25 +1,51 @@
-var app = new Vue({
-    el: '#app',
-    
-    data: {
+(function(global){
+
+    var data = {
         skillGroups:[
             {
-                title: "Programming Languages",
+                title: 'Programming Languages',
                 items:[
                     { 
-                        name: "C#",
+                        name: 'C#',
                         level: 9
                     },
                     { 
-                        name: "JavaScript",
+                        name: 'JavaScript',
                         level: 8
                     },
                     { 
-                        name: "Java",
+                        name: 'Java',
                         level: 6
                     }
-                ]
+                ]                
+            },
+            {
+                title: 'Platforms & Frameworks',
+                items:[
+                    { 
+                        name: 'ASP.Net / ASP.Net Core',
+                        level: 9
+                    },
+                    { 
+                        name: 'NodeJS',
+                        level: 7
+                    },
+                    { 
+                        name: 'Unity',
+                        level: 7
+                    }
+                ]                
             }
         ]
-    }
-  })
+    };
+
+    /* ------------------------ VUE INIT ------------------------ */
+    var app;
+    global.document.addEventListener('DOMContentLoaded', () => {
+        app = new Vue({
+            el: '#app',
+            data: data
+        });
+    } );
+
+})(window);
