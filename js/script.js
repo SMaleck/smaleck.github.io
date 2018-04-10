9ba6698a6a05121d2f72ddfd0bb1584921b02966
Sebastian Maleck <s.maleck@outlook.de>
2018-04-10 12:07:16 +0200

    Adds image and details

--- js/script.js
+++ js/script.js
@@ -1,50 +1,58 @@
 (function(global){
 
-    var data = {
-        skillGroups:[
-            {
-                title: 'Programming Languages',
-                items:[
-                    { 
-                        name: 'C#',
-                        level: 9
-                    },
-                    { 
-                        name: 'JavaScript',
-                        level: 8
-                    },
-                    { 
-                        name: 'Java',
-                        level: 6
-                    }
-                ]                
-            },
-            {
-                title: 'Platforms & Frameworks',
-                items:[
-                    { 
-                        name: 'ASP.Net / ASP.Net Core',
-                        level: 9
-                    },
-                    { 
-                        name: 'NodeJS',
-                        level: 7
-                    },
-                    { 
-                        name: 'Unity',
-                        level: 7
-                    }
-                ]                
-            }
-        ]
-    };
+    // --------------------- DETAILS
+    var details = [
+        { label: 'Race', value: 'IT-People' },
+        { label: 'Class', value: 'Developer' },
+        { label: 'Affinity', value: 'Full-Stack' }
+    ];
+
+    // --------------------- STATS
+    var stats = [
+        { label: 'Coding', value: 9 },        
+        { label: 'Willpower', value: 10 },
+        { label: 'Agility', value: 8 },
+        { label: 'Charisma', value: 7 },
+    ];
+
+    // --------------------- SKILLS
+    var skillGroups = [
+        {
+            title: 'Programming Languages',
+            items:[
+                { name: 'C#', level: 9 },
+                { name: 'JavaScript', level: 8 },
+                { name: 'Java', level: 6 }
+            ]                
+        },
+        {
+            title: 'Platforms & Frameworks',
+            items:[
+                { name: 'ASP.Net / ASP.Net Core', level: 9 },
+                { name: 'NodeJS', level: 7 },
+                { name: 'Unity', level: 7 }
+            ]                
+        }
+    ];
+
+
+
+    /* ------------------------ CHARACTER MODEL ------------------------ */
+    var characterModel = {
+        name: "Sebastian Maleck",        
+        details,
+        stats,
+        skillGroups
+    }
 
     /* ------------------------ VUE INIT ------------------------ */
     var app;
     global.document.addEventListener('DOMContentLoaded', () => {
         app = new Vue({
             el: '#app',
-            data: data
+            data: {
+                characterModel
+            }
         });
     } );
 
