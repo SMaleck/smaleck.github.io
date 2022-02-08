1b319fdf05cb940aa6c5fcd9f66f9d2ba87c3082
Sebastian Maleck <sebastian.maleck@kolibrigames.com>
2022-02-08 11:59:07 +0100

    Continues main.js

--- buildUtils/main.js
+++ buildUtils/main.js
@@ -1,27 +1,61 @@
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
 
+const basePath = '../image/crafting/';
 
-function GetFilePaths(path){
-    const entries = fs.readdirSync(path, { withFileTypes: true });
+const namingRules = {
+    sectionSplitChar: '_',
+    removeChar: '-',
+    expectedSectionCount: 3,
+    createdAtIndex: 0,
+    nameIndex: 1,
+    suffixIndex: 2,
+};
+
+function GetFilePaths(directory) {
+    const entries = fs.readdirSync(directory, { withFileTypes: true });
 
     // Get files within the current directory and add a path key to the file objects
-    const files = entries
-        .filter(file => !file.isDirectory())
-        .map(file => ({ ...file, path: path + file.name }));
-	
-    // Get folders within the current directory
-    const folders = entries.filter(folder => folder.isDirectory());
+    // const files = entries
+    //     .filter(file => !file.isDirectory())
+    //     .map(file => ({ ...file, path: path + file.name }));
 
-    for (const folder of folders)
-        /*
-          Add the found files within the subdirectory to the files array by calling the
-          current function itself
-        */
-        files.push(... GetFilePaths(`${path}${folder.name}/`));
+    const files = entries
+        .filter(entry => !entry.isDirectory())
+        .map(file => parseFileEntry(directory, file));
+
+    // Get folders within the current directory
+    const folders = entries.filter(entry => entry.isDirectory());
+
+    // Traverse subfolders recursively
+    for (const folder of folders) {
+        files.push(...GetFilePaths(`${directory}${folder.name}/`));
+    }
 
     return files;
 }
 
-var filePaths =  GetFilePaths("../image/crafting/");
+function parseFileEntry(directory, file) {
+    return {
+        meta: parseFileName(file.name),
+        fileName: file.name,
+        path: directory + file.name
+    };
+}
+
+function parseFileName(fileName) {
+    var split = fileName.split(namingRules.sectionSplitChar);
+    if (split.length !== namingRules.expectedSectionCount) {
+        throw `Filename section count invalid! Expected: ${namingRules.expectedSectionCount} | Actual: ${split.length}`;
+    }
+
+    var name = split[namingRules.nameIndex].replace(namingRules.removeChar, " ");
+
+    return {
+        createdAt: split[namingRules.createdAtIndex],
+        name: name
+    }
+}
+
+var filePaths = GetFilePaths(basePath);
 console.log(filePaths);
