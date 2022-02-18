83f2292f65047cd2e67d396e548478851d746c7a
Sebastian Maleck <sebastian.maleck@kolibrigames.com>
2022-02-18 17:40:47 +0100

    Improves sorting and styling of miniatures

--- js/miniatures.js
+++ js/miniatures.js
@@ -1,38 +1,66 @@
 
 (function (global) {
 
+    // craftingData: [{ 
+    //     category: "miniatures", 
+    //     items: [{…}]
+    // }]
+    //
     const craftingData = JSON.parse(craftingProjectsJson);
 
-    let currentCategory;
-    let currentCollection = craftingData;
-
-    function start() {
-
-        const params = new URLSearchParams(window.location.search);
-        currentCategory = params.get("category");
-
-        if (currentCategory) {
-            for (const collection of craftingData) {
-                if (collection.category === currentCategory) {
-                    currentCollection = [collection];
-                    break;
-                }
-            }
-        }
-
-        initVue();
-    }
-
     function initVue() {
+        const currentCategory = getCategory();
+        const currentCollection = getCurrentCollection(currentCategory);
+        const sortedCollection = sortByDate(currentCollection);
+
         new Vue({
             el: '#app',
             data: {
                 craftingData: craftingData,
-                collection: currentCollection
+                currentCategory: currentCategory,
+                collection: sortedCollection
             },
         });
     }
 
+    function getCategory() {
+        const params = new URLSearchParams(window.location.search);
+        return params.get("category");
+    }
+
+    function getCurrentCollection(category) {
+        if (category) {
+            for (const collection of craftingData) {
+                if (collection.category === category) {
+                    return [collection];
+                }
+            }
+        }
+
+        return GetCompleteCollection();
+    }
+
+    function GetCompleteCollection() {
+        const allItems = [];
+        for (const categoryCollection of craftingData) {
+            for (const item of categoryCollection.items) {
+                allItems.push(item);
+            }
+        }
+
+        return [{
+            category: "All",
+            items: allItems
+        }];
+    }
+
+    function sortByDate(collection) {
+        collection[0].items = collection[0].items
+            .sort((a, b) => a.createdAt < b.createdAt ? 1 : -1);
+
+        return collection;
+    }
+
     // ------------------------ INIT LISTENER
-    global.document.addEventListener('DOMContentLoaded', () => { start(); });
+    global.document.addEventListener('DOMContentLoaded', () => { initVue(); });
 })(window);
